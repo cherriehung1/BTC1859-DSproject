427fc8561a4606d0ab5e51bb2f772e830701b6e9
cherriehung1 <cherrie.hung@mail.utoronto.ca>
2026-07-26 13:09:26 -0400

    Predictors of Sleep Disturbance
     creating file for analysis of sleep disturbance predictors

---
+++ predictors_sleep_disturbance.R
@@ -0,0 +1,16 @@
+## BTC1859 - Team 1
+# Predictors of Sleep Disturbance
+
+# Own:
+#   univariable predictor analyses;
+# logistic regression;
+# adjusted models;
+# odds ratios and 95% CI;
+# model diagnostics;
+# predictor table/forest plot;
+# corresponding Methods + Results.
+# Also investigate odd issues such as renal failure having only four cases.
+# Presentation: Which patients are most likely to have sleep disturbance?
+#   
+
+# importing data in
